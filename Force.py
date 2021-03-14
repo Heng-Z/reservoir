8aac6061493727e697ff2a77781706c7be3d3f60
Heng-Z <45532139+Heng-Z@users.noreply.github.com>
2021-03-14 16:06:44 +0800

    fixed grammer bugs

--- Force.py
+++ Force.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import matlib
+import matplotlib.pyplot as plt
 def sprandn(N1,N2,p):
     # import sparse 
     import scipy.sparse as sparse
@@ -39,7 +40,8 @@ class Reservoir():
                 gz[i,j] = 1
 
         randn = g*np.random.normal(size =(self.N,Nout))
-        self.Jz = np.multiply(randn,gz)
+        self.read_connectivity = gz
+        self.Jz = np.multiply(randn,gz) #(N,Nout)
         self.read_neurons = sample_nuerons.reshape(Nout,-1)
         self.neuro_per_read = self.read_neurons.shape[1]
 
@@ -55,49 +57,56 @@ class Reservoir():
         '''
         if input_series is None:
             input_series = np.zeros(output_series.shape)
+            self.add_input(output_series.shape[0],0,0)
         assert input_series.shape[1] == output_series.shape[1]
         L = input_series.shape[1]
         Dout = output_series.shape[1]
         #array to record output trajectories during training and testing
         train_out = np.zeros((Dout,L))
         test_out = np.zeros((Dout,L))
-        x = np.random.rand(self.N)
-        r = np.random.rand(self.N)
-        Pz = repmat((1.0/aplha)*np.eye(self.neuro_per_read),self.Nout)
+        x = np.random.rand(self.N,1)
+        r = np.random.rand(self.N,1)
+        Pz = repmat((1.0/aplha)*np.eye(self.N),self.Nout)
         P = repmat((1.0/aplha)*np.eye(self.N),self.Nout,n2 = self.neuro_per_read)
         #_________________training__________________
         for i in range(L):
+            print(i)
             t = dt * i
-            x = (1.0 - dt) *x +np.dot(self.M,r*dt) + np.dot(self.Jgi,input_series[:,i]*dt)
+            x = (1.0 - dt) *x +np.dot(self.M,r*dt) + np.dot(self.Jgi,input_series[:,i]*dt).reshape(-1,1)
             r = np.tanh(x) #(N,1)
             z = np.dot(self.Jz.T,r) # (Nout,1)
 
             if np.mod(i,nt) ==0:
                 for readi in range(self.Nout):
+                    w_readi = self.Jz[:,readi]
+                    synapse_ind = np.where(w_readi!=0)
+                    flt = np.zeros((self.N,self.N))
+                    flt[synapse_ind,synapse_ind] = 1
+
                     Pzi = Pz[:,:,readi]
-                    kzi = np.dot(Pzi,r) #(Nout,1)
+                    kzi = np.dot(Pzi,np.dot(flt,r)) #(Nout,1)
                     rPr_z = np.dot(r.T,kzi) # scalar
                     c_z = 1.0/(1.0 + rPr_z) # scalar
-                    Pz[:,:,readi] = Pzi - np.dot(kzi,(kzi.T*c_z))
+                    Pz[:,:,readi] = np.dot(Pzi,flt) - np.dot(kzi,(kzi.T*c_z))
                     e_z = z[readi] - output_series[readi,i]
 
-                    dw = -e_z*kzi*c
-                    self.Jz[:,readi] += dw
+                    dw = -e_z*kzi*c_z
+                    self.Jz[:,readi] += dw.reshape(-1,)
                     
                     neurons_read_i = self.read_neurons[readi,:]
-                    for neuroni in neurons_read_i:
-                        w_ni = self.M[neuroni,:] # (1,N)
+                    for idx,neuroni in enumerate(neurons_read_i):
+                        w_ni = self.M[neuroni,:].reshape(1,-1) # (1,N)
                         synapse_ind = np.where(w_ni!=0)[1]  # find neurons pre-synaptic to neuron i 
-                        flt = np.zeros(self.N,self.N)
+                        flt = np.zeros((self.N,self.N))
                         flt[synapse_ind,synapse_ind] = 1
-                        Pi = P[:,:,readi,neuroni] #(N,N) the actual dim of Pi is num of pre-synapse
+                        Pi = P[:,:,readi,idx] #(N,N) the actual dim of Pi is num of pre-synapse
                         ki = np.dot(Pi,np.dot(flt,r)) 
                         rPr = np.dot(r.T,ki)
                         c = 1.0/(1.0 + rPr)
                         Pz[:,:,readi] = np.dot(Pi,flt) -np.dot(ki,(ki.T * c))
 
                         dw = -e_z * ki * c
-                        self.M[neuroni,:] += dw
+                        self.M[neuroni,:] += dw.reshape(-1,)
             
             train_out[:,i] = z
         #_________________testing_______________
@@ -118,17 +127,34 @@ if __name__ == "__main__":
     time_sec = 100 
     dt = 0.1
     simtime = np.arange(0,time_sec,step=dt)
-    amp = 1.3;
-    freq = 1/60;
+    simtime2 = np.arange(time_sec,2*time_sec,step=dt)
+    amp = 1.3
+    freq = 1/60
     ft = (amp/1.0)*np.sin(1.0*np.pi*freq*simtime) + \
         (amp/2.0)*np.sin(2.0*np.pi*freq*simtime) +  \
         (amp/6.0)*np.sin(3.0*np.pi*freq*simtime) +  \
         (amp/3.0)*np.sin(4.0*np.pi*freq*simtime)
-
+    ft = ft.reshape(1,-1)
+    ft2 = (amp/1.0)*np.sin(1.0*np.pi*freq*simtime2) + \
+        (amp/2.0)*np.sin(2.0*np.pi*freq*simtime2) +  \
+        (amp/6.0)*np.sin(3.0*np.pi*freq*simtime2) +  \
+        (amp/3.0)*np.sin(4.0*np.pi*freq*simtime2)
+    ft2 = ft2.reshape(1,-1)
     nn = Reservoir(N=1000,p=0.3,g=1.5)
     nn.get_Jz(1,0.3,1)
     [train_out,test_out] = nn.train(None,ft,dt,1,2)
-
+    plt.figure
+    plt.subplot(2,1,1)
+    plt.plot(simtime,ft,'b')
+    plt.subplot(2,1,2)
+    plt.plot(simtime,train_out,'r')
+    plt.figure
+    plt.subplot(2,1,1)
+    plt.plot(simtime,ft2,'b')
+    plt.subplot(2,1,2)
+    plt.plot(simtime,test_out,'g')
+    plt.show()
+    
 
                         
 
